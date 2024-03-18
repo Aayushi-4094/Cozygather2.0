3d60baa510bdb42ebefbb87f6299efb41e8eb209
Aayushi Agarwal <aa1946@srmist.edu.in>
2024-03-18 10:25:15 +0530

    Initial Commit

---
+++ cozygather2a/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cozygather2a
+//
+//  Created by user2batch2 on 18/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cozygather2a/cozygather2aApp.swift
@@ -0,0 +1,17 @@
+//
+//  cozygather2aApp.swift
+//  cozygather2a
+//
+//  Created by user2batch2 on 18/03/24.
+//
+
+import SwiftUI
+
+@main
+struct cozygather2aApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
